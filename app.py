15b4e42b679ffa2131d9a563db98a8e518cb2d47
Jack Li <xiafengli@hotmail.com>
2024-08-24 16:15:52 -0500

    update app.py and add test method

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-# from flask import Flask
-# from flask import render_template
+from flask import render_template
+from flask import Flask
+app = Flask(__name__)
 
 # app = Flask(__name__)
 
@@ -7,12 +8,9 @@
 # def hello_world():
 #     return "<p>This is Jack Li!</p>"
 
-# @app.route("/test")
-# def test():
-#     return render_template('test.html')
-
-from flask import Flask
-app = Flask(__name__)
+@app.route("/test")
+def test():
+    return render_template('test.html')
 
 @app.route('/')
 def hello_world():
